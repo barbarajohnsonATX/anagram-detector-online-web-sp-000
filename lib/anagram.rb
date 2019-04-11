2062b1c19620bfcac175721245c2f22df90bb3c1
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-11 00:39:43 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,12 +12,7 @@ class Anagram
   def match(input)
     matches_array = []
     sorted_array = []
-           temp_array = []
 
-  #  if input.include?(@word) 
-  #    matches_array << input 
-       
-  #  else 
        input.each  { |elem| sorted_array << elem.chars.sort.join }
        sorted_word = @word.chars.sort.join
        
@@ -27,8 +22,7 @@ class Anagram
           end 
         end 
    
-  #  end 
-    
+
     return matches_array
     end 
       
